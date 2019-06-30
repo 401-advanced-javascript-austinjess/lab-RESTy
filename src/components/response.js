a3b98af8cb8139773c05cfb3dfe9d7344fe48811
Austin Jess <ajess33@gmail.com>
2019-06-30 15:20:50 -0500

    json theme changes

--- src/components/response.js
+++ src/components/response.js
@@ -14,7 +14,7 @@ const Response = (props) => {
         name={null}
         collapsed={true}
         style={JsonStyle}
-        theme={JsonTheme}
+        theme={'flat'}
         displayDataTypes={true}
         displayObjectSize={true}
         src={props.response}
@@ -29,7 +29,7 @@ const NoResponseJson = () => {
       <JsonViewer
         name={null}
         style={JsonStyle}
-        theme={JsonTheme}
+        theme={'flat'}
         displayDataTypes={true}
         displayObjectSize={true}
         src={{ '': 'Enter URL and Method to begin!' }}
@@ -43,25 +43,6 @@ const JsonStyle = {
   height: '50vh',
   padding: '20px',
   borderRadius: '3px'
-};
-
-const JsonTheme = {
-  base00: 'white',
-  base01: '#1d2733',
-  base02: '#1d2733',
-  base03: '#1d2733',
-  base04: '#1d2733',
-  base05: '#444',
-  base06: '#444',
-  base07: '#444',
-  base08: '#444',
-  base09: 'rgba(70, 70, 230, 1)',
-  base0A: 'rgba(70, 70, 230, 1)',
-  base0B: 'rgba(70, 70, 230, 1)',
-  base0C: 'rgba(70, 70, 230, 1)',
-  base0D: 'rgba(70, 70, 230, 1)',
-  base0E: 'rgba(70, 70, 230, 1)',
-  base0F: 'rgba(70, 70, 230, 1)'
 };
 
 export default Response;
