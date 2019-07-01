d792c7894bdb0750aaf8bc1ea99670d197c68f2e
Keith Dahlby <keith@newbo.co>
2019-07-01 09:43:04 -0500

    Refactor to use MethodInput helper component

--- src/components/request-form/methods-input.js
+++ src/components/request-form/methods-input.js
@@ -1,25 +1,22 @@
 import React from 'react';
 
 const MethodsInput = (props) => {
+
+  const MethodInput = ({ method }) => (
+    <>
+      <input value={method} id={method} name="method" type="radio" defaultChecked={props.method === method} />
+      <label htmlFor={method}>{method.toUpperCase()}</label>
+    </>
+  )
+
   return (
     <div className="flex">
       <section className="methods">
-        <input value="get" id="get" name="method" type="radio" defaultChecked={props.method === 'get'} />
-        <label htmlFor="get" className="list-start">
-          GET
-        </label>
-
-        <input value="post" id="post" name="method" type="radio" defaultChecked={props.method === 'post'} />
-        <label htmlFor="post">POST</label>
-
-        <input value="put" id="put" name="method" type="radio" defaultChecked={props.method === 'put'} />
-        <label htmlFor="put">PUT</label>
-
-        <input value="patch" id="patch" name="method" type="radio" defaultChecked={props.method === 'patch'} />
-        <label htmlFor="patch">PATCH</label>
-
-        <input value="delete" id="delete" name="method" type="radio" defaultChecked={props.method === 'delete'} />
-        <label htmlFor="delete">DELETE</label>
+        <MethodInput method="get" />
+        <MethodInput method="post" />
+        <MethodInput method="put" />
+        <MethodInput method="patch" />
+        <MethodInput method="delete" />
       </section>
       <input type="submit" value="SEND REQUEST" />
     </div>
