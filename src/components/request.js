5da04464e445ec6452f13a381d6c011aa2aa8343
Austin Jess <ajess33@gmail.com>
2019-07-01 11:15:52 -0500

    form is now full of controlled components

--- src/components/request.js
+++ src/components/request.js
@@ -15,6 +15,7 @@ class Request extends React.Component {
     password: null,
     token: null
   };
+
   doRequest = async (e) => {
     let response;
     e.preventDefault();
@@ -23,37 +24,76 @@ class Request extends React.Component {
     this.setState({ url, method });
 
     if (method === 'post' || method === 'put') {
-      await this.postRequest(e, method, url);
+      response = await this.bodyRequest(e, method, url);
+    } else {
+      response = await axios({ method, url });
     }
-    response = await axios({ method, url });
     this.props.onResponse(response);
   };
 
-  postRequest = async (e, method, url) => {
+  bodyRequest = async (e, method, url) => {
     e.preventDefault();
     let jsonBody = e.target.body.value;
-    let response = await axios({ method, url, data: jsonBody });
-    return response;
-  };
-
-  putRequest = async (e, method, url) => {
-    e.preventDefault();
-    let jsonBody = e.target.body.value;
+    console.log(jsonBody);
     let response = await axios({ method, url, data: jsonBody });
     return response;
   };
 
   changeMethod = (method) => {
     this.setState({ method });
-  }
+  };
+
+  changeBody = (e) => {
+    console.log(e);
+    let jsonBody = e.target.value;
+    console.log(jsonBody);
+    this.setState({
+      body: jsonBody
+    });
+  };
+
+  changeUrl = (e) => {
+    let url = e.target.value;
+    this.setState({ url });
+  };
+
+  changeUsername = (e) => {
+    let username = e.target.value;
+    this.setState({ username });
+  };
+
+  changePassword = (e) => {
+    let password = e.target.value;
+    this.setState({ password });
+  };
+
+  changeToken = (e) => {
+    let token = e.target.value;
+    this.setState({ token });
+  };
 
   render() {
     return (
       <section className="resty-request">
         <form onSubmit={this.doRequest}>
-          <input name="url" type="text" placeholder="https://example.com/api" />
-          <MethodsInput method={this.state.method} onChange={this.changeMethod} />
-          <BodyHeaders {...this.state} />
+          <input
+            value={this.state.url}
+            onChange={this.changeUrl}
+            name="url"
+            type="text"
+            placeholder="https://example.com/api"
+          />
+          <MethodsInput
+            method={this.state.method}
+            onChange={this.changeMethod}
+          />
+          <BodyHeaders
+            {...this.state}
+            onBodyChange={this.changeBody}
+            onUserChange={this.changeUsername}
+            onPassChange={this.changePassword}
+            onTokenChange={this.changeToken}
+          />
         </form>
       </section>
     );
